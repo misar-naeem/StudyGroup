5a6ba3591fc659db69f37834deebcd738cf04e6e
Vinh Dinh <vinh@dinh.cc>
2022-08-22 18:35:54 +1000

    Update AWS configuration (#3)
    Run project on port 3000 to avoid blocking port 443

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,4 +10,4 @@ cd /home/ec2-user/next-app
 #install node modules
 npm install
 npm run build
-npm start -- -p 443 > app.out.log 2> app.err.log < /dev/null & 
+npm start > app.out.log 2> app.err.log < /dev/null & 

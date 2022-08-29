a2958243263b3038c7b9297f303252db7ddd17dc
SujithDach <Sujith.Dachepally@student.uts.edu.au>
2022-08-29 18:05:23 +1000

    Delete Admin Nav, because its not on the wireframes

--- pages/admin-dashboard.js
+++ pages/admin-dashboard.js
@@ -1,13 +1,12 @@
 import Head from "next/head";
 import styles from "../styles/AdminDashboard.module.css";
-import AdminNav from "../components/AdminNav";
+
 function AdminDashboard() {
   return (
     <div>
       <Head>
         <title>Admin Dashboard</title>
       </Head>
-      <AdminNav />
       <h1 className={styles.heading}>Admin Dashboard</h1>
 
       <div className={styles.subHeading}>My Classes {`:)`}</div>

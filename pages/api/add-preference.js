8ca8ab4b8e6ff0979b6fd0f8fa4f1bb6fa920a1d
tam-debug <79487142+tam-debug@users.noreply.github.com>
2022-09-01 14:46:59 +1000

    Added mongodb, add topics and student preferences functionalities. (#5)

---
+++ pages/api/add-preference.js
@@ -0,0 +1,37 @@
+import connectMongo from "../../util/mongodb";
+import Preference from "../../models/Preference";
+
+export default async function handler(req, res) {
+
+    try {
+        console.log('CONNECTING TO MONGO');
+        await connectMongo();
+        console.log('CONNECTED TO MONGO');
+
+        const query = {tutorialId: req.body.tutorialId, studentId: req.body.studentId}
+        const exists = await Preference.exists(query)
+        console.log(exists)
+
+        if (exists == null) {
+
+            console.log("CREATING PREFERENCE");
+            const preference = await Preference.create({
+                tutorialId: req.body.tutorialId,
+                studentId: req.body.studentId,
+                topic: req.body.topic,
+                degree: req.body.degree,
+                year: req.body.year
+            })
+            console.log('CREATED PREFERENCE');
+            res.json({ preference })
+
+        } else {
+            console.log('PREFERENCE ALREADY EXISTS. SKIPPING CREATION...');
+            res.json({"No":"Creation"})
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({ error });
+    }
+}
---
+++ util/mongodb.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const connectMongo = async () => mongoose.connect(process.env.MONGODB_URI);
+
+export default connectMongo;
+
+
+// import { MongoClient } from 'mongodb'
+
+// let uri = process.env.MONGODB_URI
+// let dbName = process.env.MONGODB_DB
+
+// let cachedClient = null
+// let cachedDb = null
+
+// if (!uri) {
+//   throw new Error(
+//     'Please define the MONGODB_URI environment variable inside .env.local'
+//   )
+// }
+
+// if (!dbName) {
+//   throw new Error(
+//     'Please define the MONGODB_DB environment variable inside .env.local'
+//   )
+// }
+
+// export async function connectMongo() {
+//   if (cachedClient && cachedDb) {
+//     return { client: cachedClient, db: cachedDb }
+//   }
+
+//   const client = await MongoClient.connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+
+//   console.log("Database Connected!")
+
+//   const db = await client.db(dbName)
+
+//   cachedClient = client
+//   cachedDb = db
+
+//   return { client, db }
+// }

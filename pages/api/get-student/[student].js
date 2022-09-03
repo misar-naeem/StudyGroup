9a9e94b33b9f2b8a2c81ab7139f66ac53d65a35d
vinhddinh <vinh@dinh.cc>
2022-09-04 00:13:52 +1000

    changed formatting and log level

--- pages/api/get-student/[student].js
+++ pages/api/get-student/[student].js
@@ -2,24 +2,17 @@ import connectMongo from "../../../util/mongodb";
 import Student from "../../../models/Student";
 
 export default async function handler(req, res) {
+  try {
+    const { student } = req.query;
 
-    try {
-      const { student } = req.query
+    await connectMongo();
+    console.log(`${req.url} accessed`);
 
-        console.log('CONNECTING TO MONGO');
-        await connectMongo();
-        console.log('CONNECTED TO MONGO');
-        // const email = "tamsin.low@student.uts.edu.au"
+    const result = await Student.find({ email: student });
 
-        //const result = Student.find({email: req.body.email})
-        const result = await Student.find({email: student})
-
-        console.log(result)
-
-        res.json({ result })
-
-    } catch (error) {
-        console.log(error);
-        res.json({ error });
-    }
+    res.json({ result });
+  } catch (error) {
+    console.log(error);
+    res.json({ error });
+  }
 }

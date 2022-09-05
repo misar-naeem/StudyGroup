0fa495d867209f455e07fb78fea7c7751d4ff653
Vinh Dinh <vinh@dinh.cc>
2022-09-05 22:40:53 +1000

    Added redirect on login, no page protection changes (#16)
    * added redirect on login
    
    * redirect to index if not logged in

---
+++ pages/login-redirect.js
@@ -0,0 +1,16 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+export default function LoginRedirect() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  if (session != null) {
+    if (session.user.role === "staff") {
+      router.push("/staff-dashboard");
+    } else {
+      router.push("/student-dashboard");
+    }
+  }
+  router.push("/");
+}

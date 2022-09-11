fdf4f1a141bb7b51f0766ade28a7596fb075ca33
SujithDach <Sujith.Dachepally@student.uts.edu.au>
2022-09-11 12:00:31 +1000

    Added the Student Landing page

--- pages/admin-dashboard.js
+++ pages/admin-dashboard.js
@@ -2,11 +2,9 @@ import Head from "next/head";
 import StudentOverview from '../components/StudentOverview';
 import styles from "../styles/AdminDashboard.module.css";
 
-
 function AdminDashboard() {
     
   return (
-
     <div>
       <Head>
         <title>Staff Dashboard</title>

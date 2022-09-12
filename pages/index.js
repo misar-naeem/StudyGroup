a5050d5ebe8fed3288b80382e021787b5a292a0f
SujithDach <Sujith.Dachepally@student.uts.edu.au>
2022-09-12 19:01:17 +1000

    Changes

--- pages/index.js
+++ pages/index.js
@@ -23,45 +23,6 @@ export default function Home() {
         <title>StudyGroup Landing Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      {/* <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a>Study Group</a>
-        </h1>
-
-        <p className={styles.description}>
-          An SDS Project from University of Technology Sydney
-        </p>
-
-        <div className={styles.grid}>
-          <a
-            href="#"
-            onClick={() => {
-              signIn("azure-ad", {
-                callbackUrl: "/login-redirect",
-              });
-            }}
-            className={styles.card}
-          >
-            <h2>Microsoft Login &rarr;</h2>
-            <p>
-              Sign in with Azure Active Directory to continue to SDS application
-            </p>
-          </a>
-        </div>
-        
-        <div className={styles.card}>
-          <Link href="/student-login" className={styles.card}>
-            <p>Student Login</p>
-          </Link>
-        </div>
-
-        <div className={styles.card}>
-          <Link href="/staff-login" className={styles.card}>
-            <p>Staff Login</p>
-          </Link>
-        </div>
-      </main> */}
       <div className={styles.body}>
         <main className={`${styles.main} "d-flex flex-column"`}>
           <img

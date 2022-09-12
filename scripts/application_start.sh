999701c112b3142144ce0bc1317e5824e967067b
Vinh Dinh <vinh@dinh.cc>
2022-09-13 00:39:23 +0700

    fixed typo (#25)

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,4 +10,4 @@ cd /home/ec2-user/next-app
 #install node modules
 npm ci
 npm run build
-npm start -- -p 3000> app.out.log 2> app.err.log < /dev/null & 
+npm start -- -p 3000 > app.out.log 2> app.err.log < /dev/null & 

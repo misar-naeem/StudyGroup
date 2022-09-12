29d5488dd81e1362bbc28e129207c8549f0d745e
Jasmine <79547445+JasmineBautista@users.noreply.github.com>
2022-09-12 18:35:36 +1000

    UI refactored login page
    Login page is now shared by staff and students and will be sorted accordingly

--- pages/index.js
+++ pages/index.js
@@ -20,57 +20,31 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Study Group</title>
+        <title>StudyGroup Landing Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a>Study Group</a>
-        </h1>
-
-        <p className={styles.description}>
-          An SDS Project from University of Technology Sydney
-        </p>
-
-        <div className={styles.grid}>
-          <a
-            href="#"
-            onClick={() => {
-              signIn("azure-ad", {
-                callbackUrl: "/login-redirect",
-              });
-            }}
-            className={styles.card}
-          >
-            <h2>Microsoft Login &rarr;</h2>
-            <p>
-              Sign in with Azure Active Directory to continue to SDS application
-            </p>
-          </a>
-        </div>
-        <div className={styles.card}>
-          <Link href="/admin-dashboard" className={styles.card}>
-            <p>Admin Dashboard</p>
-          </Link>
-        </div>
-        <div className={styles.card}>
-          <Link href="/student-dashboard" className={styles.card}>
-            <p>Student Dashboard</p>
-          </Link>
-        </div>
-        <div className={styles.card}>
-          <Link href="/student-login" className={styles.card}>
-            <p>Student Login</p>
-          </Link>
-        </div>
-
-        <div className={styles.card}>
-          <Link href="/staff-login" className={styles.card}>
-            <p>Staff Login</p>
-          </Link>
-        </div>
-      </main>
+      <div className={styles.body}>
+        <main className={`${styles.main} "d-flex flex-column"`}>
+          <img src="../images/darkStudyIcon.png" alt="logo icon" height="200" width="200" className={styles.logo}/>
+          <h1 className={`${styles.title} `}>
+            StudyGroup.
+          </h1>
+          <div className={styles.grid}>
+            <a
+              href="#"
+              onClick={() => {
+                signIn("azure-ad");
+              }}
+              className={styles.card}
+            >
+              <h2>Continue with Microsoft Login &rarr;</h2>
+              <p>
+                Sign in with Azure Active Directory to continue.
+              </p>
+            </a>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }

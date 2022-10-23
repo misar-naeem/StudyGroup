8e97acc091d0ed4aa4d6feec03d9d0f1f80e0e65
vinhddinh <vinh@dinh.cc>
2022-10-23 21:21:36 +1100

    Optimized api endpoint to use 2 queries

--- pages/api/get-students-in-tutorial.js
+++ pages/api/get-students-in-tutorial.js
@@ -8,25 +8,24 @@ export default async function handler(req, res) {
 
     await connectMongo();
     console.log(`${req.url} accessed`);
-    
-    var result = await Student.find({tutorials: {"$in": tutorial}});
 
-    for (var i = 0; i < result.length; i++) {
-        const preference = await Preference.find({tutorialId: tutorial, studentId: result[i]["email"]})
+    var result = await Student.find({ tutorials: { $in: tutorial } });
+    var preference = await Preference.find({ tutorialId: tutorial });
 
-        result[i] = result[i].toObject()
+    /*
+    The loop belows matches the student's email with the preference's email, and
+    adds "topic" from Preference.find() to "preference" in Student.find()
+    */
 
-        if (preference.length > 0) {
-          result[i]["preference"] = preference[0]["topic"]
-        } else {
-          result[i]["preference"] = "No Topic"
-          console.log("NOT PREFERENCE")
+    for (var i = 0; i < preference.length; i++) {
+      for (var j = 0; j < result.length; j++) {
+        if (preference[i]["studentId"] == result[j]["email"]) {
+          result[j]["preference"] = preference[i]["topic"];
         }
-
-        console.log(result)
+      }
     }
 
-    res.json({ result });
+    res.status(200).json({ result });
   } catch (error) {
     console.log(error);
     res.json({ error });
